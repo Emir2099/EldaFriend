5a00d1d8d2e19292ed879330cb1b5e5ffea574b6
virtuoso8817 <132847025+virtuoso8817@users.noreply.github.com>
2024-01-27 22:13:42 +0530

    Integrated Gemini API

---
+++ lib/geminiapi/bot.dart
@@ -0,0 +1,86 @@
+import 'dart:ffi';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart'; // Add this line to import the 'get' package
+
+import 'gemini_api.dart';
+
+final promptController = TextEditingController();
+final focusNode = FocusNode();
+
+class Bot extends StatefulWidget {
+  const Bot({Key? key}) : super(key: key);
+
+  @override
+  State<Bot> createState() => _BotState();
+}
+
+class _BotState extends State<Bot> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const BotHome(),
+    );
+  }
+}
+
+class BotHome extends StatefulWidget {
+  const BotHome({Key? key}) : super(key: key);
+
+  @override
+  _BotHomeState createState() => _BotHomeState();
+}
+
+class _BotHomeState extends State<BotHome> {
+  final textController = TextEditingController();
+  final result = ''.obs;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                keyboardType: TextInputType.text,
+                controller: textController,
+              ),
+              const SizedBox(height: 20),
+              EditableText(
+                controller: promptController,
+                focusNode: focusNode,
+                style: TextStyle(color: Colors.black),
+                backgroundCursorColor: Colors.black,
+                cursorColor: Colors.black,
+              ), // Add this line
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () async {
+                  result.value =
+                      await GeminiAPI.getGeminiData(textController.text);
+                },
+                child: const Text('Generate',
+                    style: TextStyle(color: Colors.black)),
+              ),
+              const SizedBox(height: 20),
+              Obx(() => Text(result.value,
+                  style: const TextStyle(
+                      color: Colors
+                          .black))), // Add this line to use the 'Obx' method
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:medtrack/Medicins/newMed.dart';
 import 'package:medtrack/graphs.dart';
 import 'package:medtrack/history.dart';
 import 'package:medtrack/medications.dart';
@@ -10,6 +9,7 @@ import 'package:medtrack/homePage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:medtrack/settings.dart';
 import 'package:medtrack/updateSoS.dart';
+import 'package:medtrack/geminiapi/bot.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,13 +21,13 @@ void main() async {
     print(e);
   }
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 Future<void> initializeNotifications() async {
   const AndroidInitializationSettings initializationSettingsAndroid =
       AndroidInitializationSettings('@drawable/ic_launcher');
-  final InitializationSettings initializationSettings = InitializationSettings(
+  const InitializationSettings initializationSettings = InitializationSettings(
     android: initializationSettingsAndroid,
   );
   await FlutterLocalNotificationsPlugin().initialize(
@@ -40,6 +40,8 @@ Future<void> initializeNotifications() async {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -47,20 +49,23 @@ class MyApp extends StatelessWidget {
       title: 'Stuck Service',
       initialRoute: 'MyHomePage',
       routes: {
-        'MyHomePage': (context) => MyHomePage(),
-        'register': (context) => MyRegister(),
-        'homepage': (context) => HomePage(),
-        'medications': (context) => Medications(),
-        'settings': (context) => Settings(),
-        'updateSOS': (context) => updateSOS(),
-        'graphs': (context) => graphs(),
-        'history': (context) => History()
+        'MyHomePage': (context) => const MyHomePage(),
+        'register': (context) => const MyRegister(),
+        'homepage': (context) => const HomePage(),
+        'medications': (context) => const Medications(),
+        'settings': (context) => const Settings(),
+        'updateSOS': (context) => const updateSOS(),
+        'graphs': (context) => const graphs(),
+        'history': (context) => const History(),
+        'bot': (context) => const Bot()
       },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -68,10 +73,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
         body: SafeArea(
       child: //AuthService().handleAuthState(),
-        OpenPage(),
+          Bot(),
     ));
   }
 }
